86d1e7e1ddf1ec6be1dbc245d9ea5f51a3a4f782
Lucas <lucas@agtlucas.com>
2014-12-02 20:40:08 -0200

    Updates

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,10 +1,11 @@
+// Create task collection
+Tasks = new Mongo.Collection("task");
+
 if (Meteor.isClient) {
   // Only runs on client
   Template.body.helpers({
-    tasks: [
-      { text: 'Task 1' },
-      { text: 'Task 2' },
-      { text: 'Task 3' }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
