1432c3e5664d8f0eb0a540ed28b7a70a02ba3c94
Lucas <lucas@agtlucas.com>
2014-12-02 22:39:00 -0200

    Updates

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -5,7 +5,22 @@ if (Meteor.isClient) {
   // Only runs on client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      return false;
     }
   });
 }
